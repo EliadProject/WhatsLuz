b66b405c4c46b2dd7cbcc1378ca289c1ae3219de
admin <admin@HAY-PC>
2018-08-11 13:20:54 +0300

    Added Users Managment Page

---
+++ WhatsLuzMVCAPI/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhatsLuzMVCAPI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Admin Page";
+
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            ViewBag.Title = "Users Managments";
+
+            return View();
+        }
+    }
+}
