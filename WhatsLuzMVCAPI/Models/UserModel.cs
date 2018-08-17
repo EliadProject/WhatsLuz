39da45fb0d116b0a10cb3c41546615f90c57877b
Eliad <Eliad@DESKTOP-VQ420IP>
2018-08-17 14:40:10 +0300

    Fixed CreateEvent Issue

---
+++ WhatsLuzMVCAPI/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatsLuzMVCAPI.Models
+{
+    public class UserModel
+    {
+        public string displayName { get; set; }
+        public string email { get; set; }
+        public string photoURL { get; set; }
+        public string fid { get; set; }
+
+    }
+}
