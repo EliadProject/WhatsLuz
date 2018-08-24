b5782394a2ef21926e19b86e0827658b01e3b2d2
admin <admin@HAY-PC>
2018-08-24 14:28:19 +0300

    Adding TOP 10 Places and inserted Weather API by Lat and Lang

--- WhatsLuzMVCAPI/Scripts/Statistics/statistics.js
+++ WhatsLuzMVCAPI/Scripts/Statistics/statistics.js
@@ -1,5 +1,6 @@
 ﻿
-    var newcontent = [];
+    var CatContent = [];
+    var PlacesContent = [];
 
     $.ajax({
         type: "GET",
@@ -9,7 +10,7 @@
         accepts: "application/json",
         success: function (data) {
             for (i in data) {
-                newcontent.push(
+                CatContent.push(
                {
                    label: data[i].label,
                    value: data[i].value
@@ -19,14 +20,53 @@
         }
     });
    
-    var pie = new d3pie("myPie", {
+    var pie = new d3pie("CatPie", {
         header: {
             title: {
                 text: "Events Pie by Category"
             }
         },
         data: {
-            content: newcontent
+            content: CatContent
+        },
+
+        //Here further operations/animations can be added like click event, cut out the clicked pie section.
+        callbacks: {
+            onMouseoverSegment: function (info) {
+                console.log("mouse in", info);
+            },
+            onMouseoutSegment: function (info) {
+                console.log("mouseout:", info);
+            }
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: 'http://localhost:61733/SportEvents/getTopTenPlacesStatistics',
+        dataType: 'json',
+        async: false,
+        accepts: "application/json",
+        success: function (data) {
+            for (i in data) {
+                PlacesContent.push(
+                    {
+                        label: data[i].label,
+                        value: data[i].value
+                    });
+                console.log("I'm Here!")
+            }
+        }
+    });
+
+    var pie = new d3pie("PlacesPie", {
+        header: {
+            title: {
+                text: "Events Pie by Top 10 Places"
+            }
+        },
+        data: {
+            content: PlacesContent
         },
 
         //Here further operations/animations can be added like click event, cut out the clicked pie section.
