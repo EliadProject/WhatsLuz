abf550a641bb2bf1cf11f40bbf34315c72d6d43b
admin <admin@HAY-PC>
2018-10-05 14:39:18 +0300

    added users list - view and delete

---
+++ WhatsLuzMVCAPI/Models/AdminModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatsLuzMVCAPI.Models
+{
+    public class AdminModel
+    {
+        public static List<UserAccount> getUsersList()
+        {
+            var db = new SqlConnectionDataContext();
+
+            List<UserAccount> usersList = (from myRow in db.UserAccounts
+                                           select myRow).ToList();
+
+            return usersList;
+        }
+
+        public static void removeUserByEmail(int userID)
+        {
+            var db = new SqlConnectionDataContext();
+
+            var user = db.UserAccounts.Where(u => u.UserID == userID).SingleOrDefault();
+            if (user != null)
+            {
+                db.UserAccounts.DeleteOnSubmit(user);
+                db.SubmitChanges();
+            }
+        }
+    }
+}
